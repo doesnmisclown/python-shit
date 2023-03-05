7d8f4d1a111d08abc7c0784bdaeffd1d23a8a7c6
doesnmisclown <116358854+doesnmisclown@users.noreply.github.com>
2023-03-05 11:55:12 +0300

    Add files via upload

--- ircbot.py
+++ ircbot.py
@@ -1,15 +1,19 @@
 import irc.client
 target = "#bindev"
+def validate_nickname(nick):
+  if nick.endswith("[m]"): return False
+  return True
 def on_connect(connection,event):
   if irc.client.is_channel(target):
     connection.join(target)
 def on_join(connection,event):
   print(event)
-  if not "u5er" in event.source: return
+  event.source = event.source.split("!~")[0]
+  if not validate_nickname(event.source): return
   connection.privmsg(target,f"{event.source.split('!~')[0]} вошел в чат")
 def on_quit(connection,event):
   print(event)
-  if not "u5er" in event.source: return
+  if not validate_nickname(event.source): return
   connection.privmsg(target,f"{event.source.split('!~')[0]} вышел с чата.")
 reactor = irc.client.Reactor()
 c = reactor.server().connect("irc.libera.chat",6667,"NotifyBot")
